7ba5e4a6381db4fee67c4da2b1af8b202957e6c0
Alexguti63 <amgutierrez2@buffs.wtamu.edu>
2024-06-24 09:21:42 -0500

    Add files via upload
    Homework3

--- Program.cs
+++ Program.cs
@@ -1,83 +1,56 @@
-﻿namespace Homework2;
+﻿namespace Homework3;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please input a letter grade: ");
-        string grade = Console.ReadLine();
-        if (grade == "A")
-        {
-            Console.WriteLine("GPA point: 4");
-        }
-        else if (grade == "B")
-        {
-            Console.WriteLine("GPA point: 3");
-        }
-        else if (grade == "C")
-        {
-            Console.WriteLine("GPA point: 2");
-        }
-        else if (grade == "D")
-        {
-            Console.WriteLine("GPA point: 1");
-        }
-        else if (grade == "F")
-        {
-            Console.WriteLine("GPA point: 0");
-        }
-        else
-        {
-            Console.WriteLine("Wrong letter grade!");
-        }
-        Console.WriteLine("Please input the first number: ");
-        int num1 = Convert.ToInt16(Console.ReadLine());
-        Console.WriteLine("Please input the second number: ");
-        int num2 = Convert.ToInt16(Console.ReadLine());
-        Console.WriteLine("Please input the third number: ");
-        int num3 = Convert.ToInt16(Console.ReadLine());
-        if (num1 < num2)
-        {
-            if (num1 < num3)
-            {
-                Console.WriteLine("The smallest number is: " + num1);
+        //I had help from CS50.ai to help me with Q1.
+        Console.WriteLine("Input a Integar: ");
+        int n = Convert.ToInt16(Console.ReadLine());
+        int p = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                p++;
             }
-            else
-            {
-                Console.WriteLine("The smallest number is: " + num3);
+            if(p > 2){
+                break;
             }
         }
-        else
-        {
-            if (num2 < num3)
-            {
-                Console.WriteLine("The smallest number is: " + num2);
-            }
-            else
-            {
-                Console.WriteLine("The smallest number is: " + num3);
-            }
+        if(p==2){
+            Console.WriteLine("N is a prime number");
         }
-        Console.WriteLine("Please input a year: ");
-        int year = Convert.ToInt16(Console.ReadLine());
-        if (year % 4 != 0)
-        {
-            return;
+        else{
+            Console.WriteLine("N is not a prime number");
         }
-        if (year % 100 == 0)
-        {
-            if (year % 400 == 0)
-            {
-                Console.WriteLine(year + " is a leap year.");
+
+        Console.WriteLine("Assign an int value to N: ");
+        int N = Convert.ToInt16(Console.ReadLine());
+        for(int row = 0; row < N; row++){
+            for(int col = 0; col < N; col++){
+                Console.Write('#');
             }
-            else
-            {
-                Console.WriteLine(year + " is not a leap year.");
-            }
+        Console.WriteLine("");
         }
-        else
-        {
-            Console.WriteLine(year + " is a leap year.");
+
+        Console.WriteLine("Assign an int value to N: ");
+        int R = Convert.ToInt16(Console.ReadLine());
+        for(int row = 0; row < R; row++){
+            for(int col = 0; col<R; col++){
+                if(row>=col)
+                Console.Write('*');
+            }
+        Console.WriteLine("");
+        }
+
+        Console.WriteLine("Assign an int value to N: ");
+        int B = Convert.ToInt16(Console.ReadLine());
+        for(int row = 0; row<=B; row++){
+            for(int col = 1; col <= B; col++){
+                if(row>=col)
+                Console.Write(row);
+            }
+        Console.WriteLine("");
         }
 
     }
